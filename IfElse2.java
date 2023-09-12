dc7c769545b4886693ee5a33c92cf6758f3911c9
khemlata <144773423+Khemlatabisen@users.noreply.github.com>
2023-09-12 06:34:00 -0400

    Add files via upload
    java codes

---
+++ IfElse2.java
@@ -0,0 +1,18 @@
+public class IfElse2{
+public static void main (String args[])
+{
+int a=20,b=90,c=50;
+if(a>b&&a>c)
+{
+System.out.println(a+"is greater number");
+}
+else if(b>a&&b>c){
+System.out.println(b+"is greater number");
+}
+else {
+System.out.println(c+"is greater number");
+}
+}
+}
+
+
